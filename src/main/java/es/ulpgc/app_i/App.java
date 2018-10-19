b4eded01d83c747d83b17c4af5c1e640c63aee88
Jose <alphametaghost@hotmail.com>
2018-10-19 11:13:30 +0100

    Initial Project, myapp_i

---
+++ src/main/java/es/ulpgc/app_i/App.java
@@ -0,0 +1,13 @@
+package es.ulpgc.app_i;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
